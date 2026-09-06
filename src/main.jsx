6bad9b7822f0a8609625512d3a3fdd257e6f7bee
Axelesp11 <christianaxel222@gmail.com>
2026-09-05 19:00:32 -0600

    Mount animated UIX shell

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import PwaShell from "./PwaShell.jsx";
+import UixLayer from "./UixLayer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PwaShell>
-      <App />
+      <UixLayer>
+        <App />
+      </UixLayer>
     </PwaShell>
   </StrictMode>,
 );
