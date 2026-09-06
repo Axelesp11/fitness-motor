2e07e6662f95fc1d9b6b0434a86b0a0b797a6465
Axelesp11 <christianaxel222@gmail.com>
2026-09-05 19:40:17 -0600

    UIX 4.2: cargar estilos de sesión

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppV42 from "./AppV42.jsx";
 import PwaShell from "./PwaShell.jsx";
+import "./appV42.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
