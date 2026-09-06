81de76c6f4c3423368598221a4f3a3aabd903ef5
Axelesp11 <christianaxel222@gmail.com>
2026-09-05 19:01:03 -0600

    Load UIX viewport fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import PwaShell from "./PwaShell.jsx";
 import UixLayer from "./UixLayer.jsx";
+import "./uix-patch.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
