2f5dfe4739e7bddf36cf036cf42926bd2549017b
Axelesp11 <christianaxel222@gmail.com>
2026-09-05 19:52:03 -0600

    Motor 4.4: activar AppV44

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppErrorBoundary from "./AppErrorBoundary.jsx";
-import AppV42 from "./AppV42.jsx";
+import AppV44 from "./AppV44.jsx";
 import PwaShell from "./PwaShell.jsx";
 import RuntimeGuard from "./RuntimeGuard.jsx";
 import "./appV42.css";
+import "./appV44.css";
 import "./runtime.css";
 
 createRoot(document.getElementById("root")).render(
@@ -12,7 +13,7 @@ createRoot(document.getElementById("root")).render(
     <AppErrorBoundary>
       <RuntimeGuard>
         <PwaShell>
-          <AppV42 />
+          <AppV44 />
         </PwaShell>
       </RuntimeGuard>
     </AppErrorBoundary>
