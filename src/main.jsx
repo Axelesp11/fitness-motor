60ef4b59466d312c2109f9c2817c4217c9403aea
Axelesp11 <christianaxel222@gmail.com>
2026-09-05 18:37:02 -0600

    feat: installable PWA and Android haptics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import PwaShell from "./PwaShell.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <PwaShell>
+      <App />
+    </PwaShell>
   </StrictMode>,
 );
