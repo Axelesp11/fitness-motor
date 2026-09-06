25e2e86595f34b994053308a21b5758ee1d66718
Axelesp11 <christianaxel222@gmail.com>
2026-09-05 19:38:34 -0600

    Motor 4.2: activar AppV42

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import AppV4 from "./AppV4.jsx";
+import AppV42 from "./AppV42.jsx";
 import PwaShell from "./PwaShell.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PwaShell>
-      <AppV4 />
+      <AppV42 />
     </PwaShell>
   </StrictMode>,
 );
